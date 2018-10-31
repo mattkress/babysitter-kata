86ddb974b0253fe61816676c88494392f6a01c7c
mkress <mkress@matthews-mbp-85>
2018-10-31 15:02:20 -0400

    Added first payment rate for family A

--- Pillar-Kata/src/main/java/com/pillarKata/Family.java
+++ Pillar-Kata/src/main/java/com/pillarKata/Family.java
@@ -7,6 +7,26 @@ public class Family {
 	public Family(String typeOfFamily){
 		this.typeOfFamily = typeOfFamily;
 	}
+
+	public String getTypeOfFamily() {
+		return typeOfFamily;
+	}
+
+	public void setTypeOfFamily(String typeOfFamily) {
+		this.typeOfFamily = typeOfFamily;
+	}
 	
+	public int getPaymentFromFamily(String typeOfFamily, int startTime, int endTime){
+		int moneyEarned = 0;
+		int time = startTime;
+		if(typeOfFamily.equals("A")){
+			while(time < 6 && time < endTime){
+				moneyEarned = moneyEarned + 15;
+				time++;
+			}
+		}
+		
+		return moneyEarned;
+	}
 	
 }
--- Pillar-Kata/src/test/java/com/pillarKata/FamilyTest.java
+++ Pillar-Kata/src/test/java/com/pillarKata/FamilyTest.java
@@ -1,5 +1,14 @@
 package com.pillarKata;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class FamilyTest {
 
+	@Test
+	public void getFirstPaymentRateForFamilyA(){
+		Family family = new Family("A");
+		assertEquals(family.getPaymentFromFamily("A", 0, 2), 30);		
+	}
 }
