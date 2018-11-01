48f31ee483cd3bbedf0c2626f9316fe47d971620
mkress <mkress@matthews-mbp-85>
2018-11-01 14:35:04 -0400

    Refactored getPaymentFromFamily method

--- Pillar-Kata/src/main/java/com/pillarKata/Family.java
+++ Pillar-Kata/src/main/java/com/pillarKata/Family.java
@@ -19,47 +19,13 @@ public class Family {
 	}
 	
 	public int getPaymentFromFamily(String typeOfFamily, int startTime, int endTime) throws InvalidFamilyException{
-		int moneyEarned = 0;
-		int time = startTime;
+		int moneyEarned;
 		if(typeOfFamily.equals("A")){
-			//First rate of payment before 11PM
-			while(time < 6 && time < endTime){
-				moneyEarned = moneyEarned + 15;
-				time++;
-			}
-			//Second rate of payment after 11PM
-			while(time >= 6 && time < endTime){
-				moneyEarned = moneyEarned + 20;
-				time++;
-			}
-			
+			moneyEarned = getPaymentFromFamilyA(startTime, endTime);
 		}else if(typeOfFamily.equals("B")){
-			//First rate of payment before 10PM
-			while(time < 5 && time < endTime){
-				moneyEarned = moneyEarned + 12;
-				time++;
-			}
-			//Second rate of payment between 10PM and midnight
-			while(time >= 5 && time < 7 && time < endTime){
-				moneyEarned = moneyEarned + 8;
-				time++;
-			}
-			//Third rate of payment after midnight
-			while(time >= 7 && time < endTime){
-				moneyEarned = moneyEarned + 16;
-				time++;
-			}
+			moneyEarned = getPaymentFromFamilyB(startTime, endTime);
 		}else if(typeOfFamily.equals("C")){
-			//First rate of payment before 9PM
-			while(time < 4 && time < endTime){
-				moneyEarned = moneyEarned + 21;
-				time++;
-			}
-			//Second rate of payment after 9PM
-			while(time >=4 && time < endTime){
-				moneyEarned = moneyEarned + 15;
-				time++;
-			}
+			moneyEarned = getPaymentFromFamilyC(startTime, endTime);
 		}else{
 			throw new InvalidFamilyException(typeOfFamily + "is not a valid family type");
 		}
@@ -67,4 +33,60 @@ public class Family {
 		return moneyEarned;
 	}
 	
+	
+	private int getPaymentFromFamilyA(int startTime, int endTime){
+		int moneyEarned = 0;
+		int time = startTime;
+		//First rate of payment before 11PM
+		while(time < 6 && time < endTime){
+			moneyEarned = moneyEarned + 15;
+			time++;
+		}
+		//Second rate of payment after 11PM
+		while(time >= 6 && time < endTime){
+			moneyEarned = moneyEarned + 20;
+			time++;
+		}
+		
+		return moneyEarned;
+	}
+	
+	private int getPaymentFromFamilyB(int startTime, int endTime){
+		int moneyEarned = 0;
+		int time = startTime;
+		//First rate of payment before 10PM
+		while(time < 5 && time < endTime){
+			moneyEarned = moneyEarned + 12;
+			time++;
+		}
+		//Second rate of payment between 10PM and midnight
+		while(time >= 5 && time < 7 && time < endTime){
+			moneyEarned = moneyEarned + 8;
+			time++;
+		}
+		//Third rate of payment after midnight
+		while(time >= 7 && time < endTime){
+			moneyEarned = moneyEarned + 16;
+			time++;
+		}
+		
+		return moneyEarned;
+	}
+	
+	private int getPaymentFromFamilyC(int startTime, int endTime){
+		int moneyEarned = 0;
+		int time = startTime;
+		//First rate of payment before 9PM
+		while(time < 4 && time < endTime){
+			moneyEarned = moneyEarned + 21;
+			time++;
+		}
+		//Second rate of payment after 9PM
+		while(time >=4 && time < endTime){
+			moneyEarned = moneyEarned + 15;
+			time++;
+		}
+		
+		return moneyEarned;
+	}
 }
--- Pillar-Kata/src/main/java/com/pillarKata/PayCalculator.java
+++ Pillar-Kata/src/main/java/com/pillarKata/PayCalculator.java
@@ -2,4 +2,9 @@ package com.pillarKata;
 
 public class PayCalculator {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
